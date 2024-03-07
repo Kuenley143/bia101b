0ee2cf137b6643fdbe8f5a1b93cbed3be1249751
Kuenley143 <03230143.gcbs@rub.edu.bt>
2024-03-07 14:25:52 +0600

    check the positive and negative numbers

---
+++ checkVote.py
@@ -0,0 +1,8 @@
+# ! check if user can vote
+
+# ?Get user age from input
+# ?convert userinput(str) to int() to number
+# ?check if user can vote
+    # ?if else statement
+    # ?if above 18, print "You can vote"
+    # ?if below 18, print "You canot vote"
--- positiveNegative.py
+++ positiveNegative.py
@@ -14,7 +14,16 @@
 # * check the TYPE of the input 
 # * if the type is string, how do you convert it an int?
 # * 2. check if the number is positive or negative or zero
-# ....
+# ! a == b (is it equal to)
+# ! a != b (Not equal to)
+# ! a > b (is a greater than b)
+# ! a < b (is a less than b)
+# ! a >= b (is a greater than or equal to b)
+# ! a <= b (is a less than or equal to b)
+#* - you will be comparing numbers and not string
+# ?3. Print the result
+
+
 
 # 1. get user input 
 userInput = input('Please type any number: ')
