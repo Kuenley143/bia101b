7079067992d41f3aa82268af8cbab95b59a38619
Kuenley143 <03230143.gcbs@rub.edu.bt>
2024-03-07 15:01:09 +0600

    check the strings

--- strings.py
+++ strings.py
@@ -18,3 +18,21 @@ name = "John"
 age = 30 
 interpolated = f"John is {age} years of age"
 print(interpolated)
+
+# ! string methods
+print('String Methods')
+# # ! upper
+print('String upper')
+stringlower = 'hello how are you'
+converted = stringlower.upper()
+converted1 = "pure string".upper()
+
+# ! lower
+print('String lower')
+lower = "HELLO".lower()
+print(lower)
+
+# # ! capitalize
+print('String capitalize')
+capitalize = "hello there".capitalize()
+print(capitalize)
