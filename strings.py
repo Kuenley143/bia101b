4968b163df66c593df0bc12accf985246b4ceee4
Kuenley143 <03230143.gcbs@rub.edu.bt>
2024-03-07 14:54:21 +0600

    check the strings

---
+++ strings.py
@@ -0,0 +1,20 @@
+# string manupulation 
+ 
+# string concatenation 
+string1 = "Hello"
+string2 = " World"
+
+combined = string1 + string2 
+combined1 = "hi" + " " + "there"
+
+print("string concatenation")
+print(combined)
+print(combined1)
+
+# string interpolation 
+print(" string interpolation")
+# how do you print " john is 30 years of age"
+name = "John"
+age = 30 
+interpolated = f"John is {age} years of age"
+print(interpolated)
