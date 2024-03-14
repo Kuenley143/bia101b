edc19441782eb8a6d8df317d1bfd086622f79759
Kuenley143 <03230143.gcbs@rub.edu.bt>
2024-03-14 14:38:56 +0600

    data sample

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+
+# creation of array
+array1 = [1,2,3, "thimphu", 2.5]
+
+print(array1)
+
+# retrieving 
+element1 = array1[0]
+element2 = array1[4]
+lastElement = array1[-1]
+print(lastElement)
+
+# modifying elements 
+array1[0] = 10 
+print(array1)
+
+#getting number of elements in an array
+no_of_elements = len(array1)
